12ebf2c233d5128e3a3039ae7f1c6840bccc5e27
bogdan <bogdan.denisenko2016@gmail.com>
2025-04-16 21:11:53 +0300

    #Update GenericRepository and UnitOfWork

--- Places.DAL/UnitOfWork.cs
+++ Places.DAL/UnitOfWork.cs
@@ -1,36 +1,58 @@
-using System;
-using Places.Abstract.UnitOfWork;
-using Places.Abstract.Repository;
+using Places.Abstract;
+using Places.DAL.Repositories;
 using Places.Models;
 
-namespace Places.DAL
+namespace Places.DAL.Repositories
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly PlacesDbContext _context;
+        private IRepository<Place> _placeRepository;
         private IRepository<User> _userRepository;
-        private bool _disposed = false;
+        private IRepository<Role> _roleRepository;
+        private IRepository<Review> _reviewRepository;
+        private IRepository<Question> _questionRepository;
+        private IRepository<Media> _mediaRepository;
+        private IRepository<Answer> _answerRepository;
 
         public UnitOfWork(PlacesDbContext context)
         {
             _context = context;
         }
 
-        public IRepository<User> UserRepository
+        public IRepository<Place> PlaceRepository
         {
-            get
-            {
-                if (_userRepository == null)
-                {
-                    _userRepository = new GenericRepository<User>(_context);
-                }
-                return _userRepository;
-            }
+            get { return _placeRepository ??= new GenericRepository<Place>(_context); }
         }
 
-        public IRepository<T> GetRepository<T>() where T : class
+        public IRepository<User> UserRepository
         {
-            return new GenericRepository<T>(_context);
+            get { return _userRepository ??= new GenericRepository<User>(_context); }
+        }
+
+        public IRepository<Role> RoleRepository
+        {
+            get { return _roleRepository ??= new GenericRepository<Role>(_context); }
+        }
+
+        public IRepository<Review> ReviewRepository
+        {
+            get { return _reviewRepository ??= new GenericRepository<Review>(_context); }
+        }
+
+        public IRepository<Question> QuestionRepository
+        {
+            get { return _questionRepository ??= new GenericRepository<Question>(_context); }
+        }
+
+        public IRepository<Media> MediaRepository
+        {
+            get { return _mediaRepository ??= new GenericRepository<Media>(_context); }
+        }
+
+        public IRepository<Answer> AnswerRepository
+        {
+            get { return _answerRepository ??= new GenericRepository<Answer>(_context); }
         }
 
         public void SaveChanges()
@@ -38,22 +60,9 @@ namespace Places.DAL
             _context.SaveChanges();
         }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (disposing)
-                {
-                    _context.Dispose();
-                }
-                _disposed = true;
-            }
-        }
-
         public void Dispose()
         {
-            Dispose(true);
-            GC.SuppressFinalize(this);
+            _context.Dispose();
         }
     }
 }
