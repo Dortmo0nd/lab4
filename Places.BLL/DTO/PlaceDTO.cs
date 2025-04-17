f973d634f1ed4fe1e6cf74ee9aa9376230d6bd9b
bogdan <bogdan.denisenko2016@gmail.com>
2025-04-17 10:35:32 +0300

    #update

--- Places.BLL/DTO/PlaceDTO.cs
+++ Places.BLL/DTO/PlaceDTO.cs
@@ -7,6 +7,5 @@ namespace Places.BLL.DTO
         public string Description { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        // Додайте інші властивості за потреби
     }
 }
