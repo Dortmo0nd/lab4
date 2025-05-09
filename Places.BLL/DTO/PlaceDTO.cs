8144172d15667243d4e888d112421e211e97d567
bogdan <bogdan.denisenko2016@gmail.com>
2025-05-09 18:06:19 +0300

    #Added new place

--- Places.BLL/DTO/PlaceDTO.cs
+++ Places.BLL/DTO/PlaceDTO.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Places.BLL.DTO
 {
     public class PlaceDTO
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public List<ReviewDTO> Reviews { get; set; } = new List<ReviewDTO>();
+        public List<QuestionDTO> Questions { get; set; } = new List<QuestionDTO>();
+        public List<MediaDTO> MediaFiles { get; set; } = new List<MediaDTO>();
     }
 }
--- Places.WebAPI/Program.cs
+++ Places.WebAPI/Program.cs
@@ -14,16 +14,21 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Настройка контроллеров и сериализации JSON
 builder.Services.AddControllersWithViews().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
+
+// Подключение Swagger для API
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Настройка контекста базы данных с использованием SQLite
 builder.Services.AddDbContext<PlacesDbContext>(options =>
-    options.UseSqlite("Data Source=places.db").UseLazyLoadingProxies());
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Настройка аутентификации через cookies
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
@@ -31,6 +36,7 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         //options.AccessDeniedPath = "/Users/AccessDenied";
     });
 
+// Регистрация сервисов и репозиториев
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IPlaceService, PlaceService>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -39,6 +45,7 @@ builder.Services.AddScoped<IQuestionService, QuestionService>();
 builder.Services.AddScoped<IMediaService, MediaService>();
 builder.Services.AddScoped<IAnswerService, AnswerService>();
 
+// Регистрация мапперов
 builder.Services.AddScoped<PlaceMapper>();
 builder.Services.AddScoped<UserMapper>();
 builder.Services.AddScoped<ReviewMapper>();
@@ -46,25 +53,32 @@ builder.Services.AddScoped<QuestionMapper>();
 builder.Services.AddScoped<MediaMapper>();
 builder.Services.AddScoped<AnswerMapper>();
 
+// Подключение Razor Pages
 builder.Services.AddRazorPages();
 
+// Создание приложения
 var app = builder.Build();
 
+// Настройка middleware для разработки
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Настройка обработки статических файлов, маршрутизации и авторизации
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+// Настройка маршрута по умолчанию
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Auth}/{action=Index}/{id?}");
 
+// Подключение Razor Pages
 app.MapRazorPages();
 
+// Запуск приложения
 app.Run();
