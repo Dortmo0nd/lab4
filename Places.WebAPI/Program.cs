3b23002f59704089b023d9f753253cc7f334a9d2
bogdan <bogdan.denisenko2016@gmail.com>
2025-05-16 11:23:08 +0300

    #Update program.cs

--- Places.WebAPI/Program.cs
+++ Places.WebAPI/Program.cs
@@ -2,38 +2,17 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Places.Infrastructure;
 using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Localization;
-using System.Globalization;
-using Microsoft.AspNetCore.Mvc.Razor;
+
 
 var builder = WebApplication.CreateBuilder(args);
+PlacesDalDependencyRegistration.AddPlacesDal(builder.Services, "Data Source=places.db");
 
-// Реєстрація DAL
-builder.Services.AddPlacesDal("Data Source=places.db");
 
-// Реєстрація BLL
 builder.Services.AddPlacesBll();
-
-// Додавання служб локалізації
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-
-// Налаштування підтримуваних культур
-var supportedCultures = new[] { "en-US", "uk-UA" };
-builder.Services.Configure<RequestLocalizationOptions>(options =>
+builder.Services.AddControllersWithViews().AddJsonOptions(options =>
 {
-    options.DefaultRequestCulture = new RequestCulture("uk-UA");
-    options.SupportedCultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
-    options.SupportedUICultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
-
-// Реєстрація контролерів із підтримкою локалізації представлень
-builder.Services.AddControllersWithViews()
-    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix) // Додає підтримку IViewLocalizer
-    .AddDataAnnotationsLocalization() // Локалізація повідомлень валідації
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -45,21 +24,14 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     });
 
 builder.Services.AddRazorPages();
-
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseStaticFiles();
 app.UseRouting();
-
-// Додавання middleware для локалізації
-app.UseRequestLocalization();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
